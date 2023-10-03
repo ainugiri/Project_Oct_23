bd9e2be0c03c8f86d2b381c7e2499b339db6ef2c
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-10-03 17:26:30 +0530

    Initial Coomit

---
+++ src/main/java/com/Sample2.java
@@ -0,0 +1,15 @@
+class Sample2{
+
+public void display(){
+System.out.println("Hi");
+}
+
+public String createMaterialNumber(int x, int y, String abc){
+	int portNo = x;
+	int areaCode =y;
+	String serviceName = abc;
+	return abc;
+}
+
+
+}
